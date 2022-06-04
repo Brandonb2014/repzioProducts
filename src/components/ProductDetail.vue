4d5c245ff71447667895b0dc6ae63eabe3d23dbb
Brandon B <brandon.birschbach@gmail.com>
2022-06-04 15:04:53 -0600

    Set up ProductListItem and ProductDetail components

---
+++ src/components/ProductDetail.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="product-details-wrapper">
+    <div @click="closeModal">X</div>
+    <img class="thumbnail" :src="photoName + '?w=170&&crop=10,10,-10,-10'" />
+    <div class="item-details">
+      <div class="item-name">{{ itemName }}</div>
+      <div>ID: {{ itemId }}</div>
+      <div>{{ formatPrice }}</div>
+      <div>{{ description }}</div>
+      <div>{{ dimensions }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetail',
+  props: {
+    itemId: String,
+    photoName: String,
+    itemName: String,
+    description: String,
+    dimensions: String,
+    basePrice: Number,
+  },
+
+  computed: {
+    formatPrice() {
+      return '$' + this.basePrice.toFixed(2);
+    }
+  },
+
+  methods: {
+    closeModal() {
+      this.$emit('closeModal');
+    }
+  },
+}
+</script>
+
+<style scoped>
+.product-details-wrapper {
+  position: fixed;
+  width: 90vw;
+  height: 90vh;
+  top: 2rem;
+  left: 3rem;
+  background: white;
+  border: 1px solid black;
+  border-radius: 15px;
+  transition: all .5s;
+}
+</style>
---
+++ src/components/ProductListItem.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="item-wrapper" @click="showDetail = !showDetail">
+    <img class="thumbnail" :src="photoName + '?crop=10,10,-10,-10'" />
+  </div>
+
+  <ProductDetail
+    v-show="showDetail"
+    v-on:closeModal="closeModal()"
+    :itemId="itemId"
+    :photoName="photoName"
+    :itemName="itemName"
+    :description="description"
+    :dimensions="dimensions"
+    :basePrice="basePrice">
+  </ProductDetail>
+</template>
+
+<script>
+import ProductDetail from './ProductDetail.vue';
+
+export default {
+  name: 'ProductListItem',
+  components: {
+    ProductDetail
+  },
+
+  props: {
+    itemId: String,
+    photoName: String,
+    itemName: String,
+    description: String,
+    dimensions: String,
+    basePrice: Number,
+  },
+
+  data() {
+    return {
+      showDetail: false,
+    }
+  },
+
+  methods: {
+    closeModal() {
+      this.showDetail = false;
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.item-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  margin: 15px 0;
+  cursor: pointer;
+  border: 1px solid transparent;
+  border-radius: 15px;
+  transition: all .5s;
+
+  &:hover {
+    border: 1px solid black;
+  }
+}
+.thumbnail {
+  width: 80px;
+  max-height: 200px;
+}
+
+
+
+@media (min-width: 525px) {
+  .thumbnail {
+    max-width: 210px;
+  }
+}
+</style>
